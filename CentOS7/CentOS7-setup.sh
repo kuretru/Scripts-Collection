4cf25ce44aec976890c9622f52d6ae67c4dca3eb
呉真 <kuretru@gmail.com>
2019-10-12 10:38:57 +0800

    修改：重新命名，支持多操作系统版本

--- CentOS7/CentOS7-setup.sh
+++ CentOS7/CentOS7-setup.sh
@@ -233,8 +233,8 @@ EOF
     wget https://raw.githubusercontent.com/kuretru/Scripts-Collection/master/files/nginx/security.conf -O security.conf
     wget https://raw.githubusercontent.com/kuretru/Scripts-Collection/master/files/nginx/proxy.conf -O proxy.conf
     ce /etc/nginx/modules
-    wget https://raw.githubusercontent.com/kuretru/Scripts-Collection/master/files/nginx/ngx_http_brotli_filter_module.so -O ngx_http_brotli_filter_module.so
-    wget https://raw.githubusercontent.com/kuretru/Scripts-Collection/master/files/nginx/ngx_http_brotli_static_module.so -O ngx_http_brotli_static_module.so
+    wget https://raw.githubusercontent.com/kuretru/Scripts-Collection/master/files/nginx/ngx_http_brotli_filter_module.so.el7 -O ngx_http_brotli_filter_module.so
+    wget https://raw.githubusercontent.com/kuretru/Scripts-Collection/master/files/nginx/ngx_http_brotli_static_module.so.el7 -O ngx_http_brotli_static_module.so
     chmod 755 *.so
     cd /etc/nginx/conf.d
     mv default.conf default.conf.bk
